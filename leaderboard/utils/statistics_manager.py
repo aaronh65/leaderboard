a3766b113717e517815a10b52d435754f22b13b0
aaron <aaronh65@gmail.com>
2021-01-27 11:52:45 -0800

    moved plot code outside of statistics manager to minimize leaderboard code interference

--- leaderboard/utils/statistics_manager.py
+++ leaderboard/utils/statistics_manager.py
@@ -12,44 +12,19 @@ This module contains a statistics manager for the CARLA AD leaderboard
 from __future__ import print_function
 
 from dictor import dictor
-from collections import deque
 import math
-import os,sys
-
-import matplotlib.pyplot as plt
-from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
-import numpy as np
-import seaborn as sns
-colors = sns.color_palette("Paired")
+import sys
 
 from srunner.scenariomanager.traffic_events import TrafficEventType
-from leaderboard.scenarios.route_scenario import NUMBER_CLASS_TRANSLATION
-from leaderboard.utils.checkpoint_tools import fetch_dict, save_dict, create_default_json_msg
 
-SAVE_ROOT = os.environ.get('SAVE_ROOT', 0)
-ROUTE_NAME = os.environ.get('ROUTE_NAME', 0)
+from leaderboard.utils.checkpoint_tools import fetch_dict, save_dict, create_default_json_msg
+from leaderboard.utils.statistics_util import plot_performance
 
 PENALTY_COLLISION_PEDESTRIAN = 0.50
 PENALTY_COLLISION_VEHICLE = 0.60
 PENALTY_COLLISION_STATIC = 0.65
 PENALTY_TRAFFIC_LIGHT = 0.70
 PENALTY_STOP = 0.80
-
-penalty_dict = {
-        TrafficEventType.COLLISION_PEDESTRIAN  : PENALTY_COLLISION_PEDESTRIAN,
-        TrafficEventType.COLLISION_VEHICLE  : PENALTY_COLLISION_VEHICLE,
-        TrafficEventType.COLLISION_STATIC  : PENALTY_COLLISION_STATIC,
-        TrafficEventType.TRAFFIC_LIGHT_INFRACTION  : PENALTY_TRAFFIC_LIGHT,
-        TrafficEventType.STOP_INFRACTION  : PENALTY_STOP
-        }
-
-string_dict = {
-        TrafficEventType.COLLISION_PEDESTRIAN  : f'hit ped ({PENALTY_COLLISION_PEDESTRIAN}x)',
-        TrafficEventType.COLLISION_VEHICLE  : f'hit vehicle ({PENALTY_COLLISION_VEHICLE}x)',
-        TrafficEventType.COLLISION_STATIC  : f'hit static ({PENALTY_COLLISION_STATIC}x)',
-        TrafficEventType.TRAFFIC_LIGHT_INFRACTION  : f'ran light ({PENALTY_TRAFFIC_LIGHT}x)',
-        TrafficEventType.STOP_INFRACTION  : f'ran stop ({PENALTY_STOP}x)',
-        }
 
 
 class RouteRecord():
@@ -147,84 +122,6 @@ class StatisticsManager(object):
         self._route_scenario = scenario
         self._master_scenario = scenario.scenario
 
-
-    def plot_performance(self, score_route_list, infraction_list, checkpoint, tol=1e-4):
-        
-        fig = plt.gcf()
-        fig.set_size_inches(12,8)
-        ax = plt.gca()
-        
-        # compute penalties
-        infraction_list = sorted(infraction_list, key=lambda x: x[0])
-        inf_time_mult = deque([(time, penalty_dict[itype]) for time, itype in infraction_list])
-        score_penalty = [1.0] * len(score_route_list)
-        for i in range(1, len(score_penalty)):
-
-            score_penalty[i] = score_penalty[i-1]
-            if len(inf_time_mult) == 0:
-                continue
-
-            # check for active infraction and apply penalty if so
-            inf_time, penalty = inf_time_mult[0]
-            if abs(i*0.05 - inf_time) < tol or i*0.05 - inf_time >= 0.05:
-                score_penalty[i] = score_penalty[i-1]*penalty
-                inf_time_mult.popleft()
-
-        # compute driving scores and reduce to 2 Hz
-        score_composed_list = np.multiply(score_penalty, score_route_list) # 20 Hz
-        score_composed_plot = score_composed_list[::10] # 2 Hz
-        score_route_plot = score_route_list[::10]
-
-        # plot scores
-        x_plot = np.arange(len(score_composed_plot)) * 0.5 # 2 Hz
-        plt.plot(x_plot, score_route_plot, label='route completion', color=colors[0])
-        plt.plot(x_plot, score_composed_plot, label='driving score', color=colors[2])
-
-        # useful for plotting
-        xmin, xmax = plt.xlim()
-        ymin, ymax = plt.ylim()
-        increment = ymax/30
-        repeat = 8
-
-        # plot infraction times
-        for i, (time, itype) in enumerate(infraction_list):
-            offset = increment*(i%repeat + 1) # extra 1 so text shows up below the top of the plot
-            plt.vlines(time, ymin, ymax, linestyles='dashed', alpha=0.5, color='red')
-            plt.text(time+0.2, ymax-offset, string_dict[itype])
-
-        # plot scenario trigger times
-        scenarios = self._route_scenario.scenario_triggerer._triggered_scenarios
-        times = self._route_scenario.scenario_triggerer._triggered_scenarios_times
-        lookup = self._route_scenario.route_var_name_class_lookup
-        for i, (time, route_var_name) in enumerate(zip(times, scenarios)):
-            offset = increment*(i%repeat)
-            plt.vlines(time, ymin, ymax, linestyles='dashed', alpha=0.5, color='purple')
-            plt.text(time+0.2, ymin+offset+ymax/60, lookup[route_var_name])
-        
-        # label axes and format ticks
-        plt.xlabel('Game time')
-        plt.ylabel('Score (%)')
-        def format_ticks(value, tick_number):
-            minute = int(value/60)
-            return f'{minute:02d}:00'
-        ax.xaxis.set_major_locator(MultipleLocator(60))
-        ax.xaxis.set_major_formatter(plt.FuncFormatter(format_ticks))
-        ax.xaxis.set_minor_locator(MultipleLocator(15))
-        ax.tick_params(which='both', direction='in')
-        ax.set_xlim([xmin, xmax])
-        ax.set_ylim([ymin, ymax])
-        
-        # finish up and save
-        rep_number = int(os.environ.get('REP', 0))
-        split = SAVE_ROOT.split('/')[-1]
-        save_path = f'{SAVE_ROOT}/plots/{ROUTE_NAME}/repetition_{rep_number:02d}.png'
-        title = f'{split}/{ROUTE_NAME}: repetition {rep_number:02d}'
-        title = title.replace('_', ' ')
-        plt.title(title)
-        plt.legend(frameon=False, loc='lower right')
-        plt.savefig(save_path)
-        plt.clf()
-
     def compute_route_statistics(self, config, duration_time_system=-1, duration_time_game=-1, failure="", checkpoint=None):
         """
         Compute the current statistics by evaluating all relevant scenario criteria
@@ -313,7 +210,7 @@ class StatisticsManager(object):
 
         # plot per-route performance
         if len(score_route_list) > 0:
-            self.plot_performance(score_route_list, infraction_list, checkpoint)
+            plot_performance(score_route_list, infraction_list, checkpoint, self._route_scenario.scenario_triggerer, self._route_scenario.route_var_name_class_lookup)
         
         # update status
         if target_reached:
---
+++ leaderboard/utils/statistics_util.py
@@ -0,0 +1,113 @@
+import os
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from collections import deque
+from matplotlib.ticker import (MultipleLocator, AutoMinorLocator)
+from srunner.scenariomanager.traffic_events import TrafficEventType
+
+colors = sns.color_palette("Paired")
+SAVE_ROOT = os.environ.get('SAVE_ROOT', 0)
+ROUTE_NAME = os.environ.get('ROUTE_NAME', 0)
+
+PENALTY_COLLISION_PEDESTRIAN = 0.50
+PENALTY_COLLISION_VEHICLE = 0.60
+PENALTY_COLLISION_STATIC = 0.65
+PENALTY_TRAFFIC_LIGHT = 0.70
+PENALTY_STOP = 0.80
+
+penalty_dict = {
+        TrafficEventType.COLLISION_PEDESTRIAN  : PENALTY_COLLISION_PEDESTRIAN,
+        TrafficEventType.COLLISION_VEHICLE  : PENALTY_COLLISION_VEHICLE,
+        TrafficEventType.COLLISION_STATIC  : PENALTY_COLLISION_STATIC,
+        TrafficEventType.TRAFFIC_LIGHT_INFRACTION  : PENALTY_TRAFFIC_LIGHT,
+        TrafficEventType.STOP_INFRACTION  : PENALTY_STOP
+        }
+
+string_dict = {
+        TrafficEventType.COLLISION_PEDESTRIAN  : f'hit ped ({PENALTY_COLLISION_PEDESTRIAN}x)',
+        TrafficEventType.COLLISION_VEHICLE  : f'hit vehicle ({PENALTY_COLLISION_VEHICLE}x)',
+        TrafficEventType.COLLISION_STATIC  : f'hit static ({PENALTY_COLLISION_STATIC}x)',
+        TrafficEventType.TRAFFIC_LIGHT_INFRACTION  : f'ran light ({PENALTY_TRAFFIC_LIGHT}x)',
+        TrafficEventType.STOP_INFRACTION  : f'ran stop ({PENALTY_STOP}x)',
+        }
+
+def plot_performance(score_route_list, infraction_list, checkpoint, scenario_triggerer, route_var_name_class_lookup, tol=1e-4):
+        
+    fig = plt.gcf()
+    fig.set_size_inches(12,8)
+    ax = plt.gca()
+    
+    # compute penalties
+    infraction_list = sorted(infraction_list, key=lambda x: x[0])
+    inf_time_mult = deque([(time, penalty_dict[itype]) for time, itype in infraction_list])
+    score_penalty = [1.0] * len(score_route_list)
+    for i in range(1, len(score_penalty)):
+
+        score_penalty[i] = score_penalty[i-1]
+        if len(inf_time_mult) == 0:
+            continue
+
+        # check for active infraction and apply penalty if so
+        inf_time, penalty = inf_time_mult[0]
+        if abs(i*0.05 - inf_time) < tol or i*0.05 - inf_time >= 0.05:
+            score_penalty[i] = score_penalty[i-1]*penalty
+            inf_time_mult.popleft()
+
+    # compute driving scores and reduce to 2 Hz
+    score_composed_list = np.multiply(score_penalty, score_route_list) # 20 Hz
+    score_composed_plot = score_composed_list[::10] # 2 Hz
+    score_route_plot = score_route_list[::10]
+
+    # plot scores
+    x_plot = np.arange(len(score_composed_plot)) * 0.5 # 2 Hz
+    plt.plot(x_plot, score_route_plot, label='route completion', color=colors[0])
+    plt.plot(x_plot, score_composed_plot, label='driving score', color=colors[2])
+
+    # useful for plotting
+    xmin, xmax = plt.xlim()
+    ymin, ymax = plt.ylim()
+    increment = ymax/30
+    repeat = 8
+
+    # plot infraction times
+    for i, (time, itype) in enumerate(infraction_list):
+        offset = increment*(i%repeat + 1) # extra 1 so text shows up below the top of the plot
+        plt.vlines(time, ymin, ymax, linestyles='dashed', alpha=0.5, color='red')
+        plt.text(time+0.2, ymax-offset, string_dict[itype])
+
+    # plot scenario trigger times
+    scenarios = scenario_triggerer._triggered_scenarios
+    times = scenario_triggerer._triggered_scenarios_times
+    lookup = route_var_name_class_lookup
+    for i, (time, route_var_name) in enumerate(zip(times, scenarios)):
+        offset = increment*(i%repeat)
+        plt.vlines(time, ymin, ymax, linestyles='dashed', alpha=0.5, color='purple')
+        plt.text(time+0.2, ymin+offset+ymax/60, lookup[route_var_name])
+    
+    # label axes and format ticks
+    plt.xlabel('Game time')
+    plt.ylabel('Score (%)')
+    def format_ticks(value, tick_number):
+        minute = int(value/60)
+        return f'{minute:02d}:00'
+    ax.xaxis.set_major_locator(MultipleLocator(60))
+    ax.xaxis.set_major_formatter(plt.FuncFormatter(format_ticks))
+    ax.xaxis.set_minor_locator(MultipleLocator(15))
+    ax.tick_params(which='both', direction='in')
+    ax.set_xlim([xmin, xmax])
+    ax.set_ylim([ymin, ymax])
+    
+    # finish up and save
+    rep_number = int(os.environ.get('REP', 0))
+    split = SAVE_ROOT.split('/')[-1]
+    save_path = f'{SAVE_ROOT}/plots/{ROUTE_NAME}/repetition_{rep_number:02d}.png'
+    title = f'{split}/{ROUTE_NAME}: repetition {rep_number:02d}'
+    title = title.replace('_', ' ')
+    plt.title(title)
+    plt.legend(frameon=False, loc='lower right')
+    plt.savefig(save_path)
+    plt.clf()
+
+
