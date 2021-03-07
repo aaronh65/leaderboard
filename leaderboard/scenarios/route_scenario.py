a5acbe63f69f52af18aa4a5f6cefacc1a62d6e89
aaron <aaronh65@gmail.com>
2021-03-07 11:42:34 -0800

    route scenario decrease to route deviation threshold

--- leaderboard/scenarios/route_scenario.py
+++ leaderboard/scenarios/route_scenario.py
@@ -542,7 +542,8 @@ class RouteScenario(BasicScenario):
 
         route_criterion = InRouteTest(self.ego_vehicles[0],
                                       route=route,
-                                      offroad_max=30,
+                                      #offroad_max=30,
+                                      offroad_max=10,
                                       terminate_on_failure=True)
                                       
         completion_criterion = RouteCompletionTest(self.ego_vehicles[0], route=route)
